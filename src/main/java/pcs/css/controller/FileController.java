473c7032e9b01ae0e0a5d202eeba904885aa3f85
Lim <gustjr790@gmail.com>
2022-12-05 16:39:48 +0900

    2022_12_05 deploy complete

--- src/main/java/pcs/css/controller/FileController.java
+++ src/main/java/pcs/css/controller/FileController.java
@@ -8,24 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import pcs.css.dto.FileDTO;
 import pcs.css.dto.FileResponse;
 import pcs.css.service.impl.FileService;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.util.Objects;
-import java.util.UUID;
-
-
 
 
 @CrossOrigin(origins = "*")
